cec0fe3993c402631455f7c2779ed02b7311ef7c
Waihon Yew <yewwaihon@gmail.com>
2021-07-13 23:05:02 +0800

    Write a feature spec for viewing the list of movies

---
+++ spec/features/list_movies_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+describe "Viewing the list of movies" do
+  
+  before do
+    Movie.create(title: "Movie 1")
+    Movie.create(title: "Movie 2")
+    Movie.create(title: "Movie 3")    
+  end
+
+  it "shows the movies" do
+    visit 'http://example.com/movies'
+
+    expect(page).to have_text("3 Movies")
+    expect(page).to have_text("Movie 1")
+    expect(page).to have_text("Movie 2")
+    expect(page).to have_text("Movie 3")
+  end
+
+end
