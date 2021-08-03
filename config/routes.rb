ce3838fa89475ddb2fc7ad9d42063f01e09f3a48
Waihon Yew <yewwaihon@gmail.com>
2021-08-03 22:24:37 +0800

    Add a route for new movie

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "movies#index"
   get "movies" => "movies#index"
+  get "movies/new" => "movies#new", as: "new_movie"
   get "movies/:id" => "movies#show", as: "movie"
   get "movies/:id/edit" => "movies#edit", as: "edit_movie"
   patch "movies/:id" => "movies#update"
