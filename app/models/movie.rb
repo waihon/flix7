7514e4e1e7cdb48702d0032986459661aa39a110
Waihon Yew <yewwaihon@gmail.com>
2021-08-06 00:00:25 +0800

    Return movies with at least 300M total gross, ordered with the highest grossing

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,6 +3,10 @@ class Movie < ApplicationRecord
     Movie.where("released_on < ?", Time.now).order(released_on: :desc)
   end
 
+  def self.hits
+    Movie.where("total_gross >= ?", 300_000_000).order(total_gross: :desc)
+  end
+
   def flop?
     total_gross.blank? || total_gross < 225_000_000
   end
