75897ed7c0fcfb17e345751a263b53f4e1f8f6e7
Waihon Yew <yewwaihon@gmail.com>
2021-08-06 00:11:04 +0800

    Return the last 3 movies ordered with the most recently-added

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -11,6 +11,10 @@ class Movie < ApplicationRecord
     Movie.where("total_gross < ?", 225_000_000).order(total_gross: :asc)
   end
 
+  def self.recently_added
+    order(created_at: :desc).limit(3)
+  end
+
   def flop?
     total_gross.blank? || total_gross < 225_000_000
   end
