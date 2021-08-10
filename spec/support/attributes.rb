3247f487ff5a2591cc1dfb857d56c0cce8deefca
Waihon Yew <yewwaihon@gmail.com>
2021-08-10 20:55:15 +0800

    Update movie_attributes with additional fields

--- spec/support/attributes.rb
+++ spec/support/attributes.rb
@@ -4,6 +4,9 @@ def movie_attributes(overrides = {})
     rating: "PG-13",
     total_gross: 318_412_101.00,
     description: "Tony Stark builds an armored suit to fight the throes of evil",
-    released_on: "2008-05-02"
+    released_on: "2008-05-02",
+    director: "Jon Favreau",
+    duration: "126 min",
+    image_file_name: "ironman.jpg"
   }.merge(overrides)
 end
