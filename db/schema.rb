819f93697b36bec34dfad0dd38fe6c1156bdc2b5
Waihon Yew <yewwaihon@gmail.com>
2021-08-10 21:33:51 +0800

    schema.rb is auto updated with additional fields

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_11_025602) do
+ActiveRecord::Schema.define(version: 2021_08_10_132534) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,9 @@ ActiveRecord::Schema.define(version: 2021_07_11_025602) do
     t.datetime "updated_at", precision: 6, null: false
     t.text "description"
     t.date "released_on"
+    t.string "director"
+    t.string "duration"
+    t.string "image_file_name", default: "placeholder.png"
   end
 
 end
