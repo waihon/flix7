89ef4a76004e8e8a2fef9ed83c4ec04ac01c9572
Waihon Yew <yewwaihon@gmail.com>
2021-08-10 21:29:07 +0800

    Set default value of image_file_name column to placeholder.png

--- db/migrate/20210810132534_add_more_fields_to_movies.rb
+++ db/migrate/20210810132534_add_more_fields_to_movies.rb
@@ -2,6 +2,6 @@ class AddMoreFieldsToMovies < ActiveRecord::Migration[6.1]
   def change
     add_column :movies, :director, :string
     add_column :movies, :duration, :string
-    add_column :movies, :image_file_name, :string
+    add_column :movies, :image_file_name, :string, default: "placeholder.png"
   end
 end
