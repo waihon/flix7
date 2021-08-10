e83dbe76a381a4832a5e8d8117b9da1d685dd227
Waihon Yew <yewwaihon@gmail.com>
2021-08-10 20:52:13 +0800

    Update create movie spec to include additional fields

--- spec/features/create_movie_spec.rb
+++ spec/features/create_movie_spec.rb
@@ -12,6 +12,9 @@ describe "Creating a new movie" do
     fill_in "Rating", with: "PG-13"
     fill_in "Total gross", with: "75000000"
     select (Time.now.year - 1).to_s, :from => "movie_released_on_1i"
+    fill_in "Director", with: "The ever-creative director"
+    fill_in "Duration", with: "123 min"
+    fill_in "Image file name", with: "movie.png"
 
     # If you're taking advantage of the HTML 5 date field in Chrome,
     # you'll need to use 'fill_in' rather than 'select'.
