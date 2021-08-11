bec49b53aa61e9756509f91420559d9a912f12b0
Waihon Yew <yewwaihon@gmail.com>
2021-08-11 12:21:01 +0800

    Movie accepts any reating that is in an approved list

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -9,6 +9,8 @@ class Movie < ApplicationRecord
     with: /\w+\.(jpg|png)\z/i,
     message: "must be a JPG or PNG image"
   }
+  RATINGS = %w[G PG PG-13 R NC-17]
+  validates :rating, inclusion: { in: RATINGS }
 
   def self.released
     Movie.where("released_on < ?", Time.now).order(released_on: :desc)
