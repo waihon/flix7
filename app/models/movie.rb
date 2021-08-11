52962e365f40c9fa54a610140735c2a90868b16c
Waihon Yew <yewwaihon@gmail.com>
2021-08-11 12:01:00 +0800

    Movie requires a released on date

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,7 @@
 class Movie < ApplicationRecord
   validates :title, presence: true
+  validates :description, presence: true
+  validates :released_on, presence: true
 
   def self.released
     Movie.where("released_on < ?", Time.now).order(released_on: :desc)
