a3267a6d87c3b5fbc393578d8cbff3b489ec47b5
Waihon Yew <yewwaihon@gmail.com>
2021-08-11 12:03:45 +0800

    Movie requires a duration

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,6 +2,7 @@ class Movie < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true
   validates :released_on, presence: true
+  validates :duration, presence: true
 
   def self.released
     Movie.where("released_on < ?", Time.now).order(released_on: :desc)
