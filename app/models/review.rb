45c1f3aee454bd6fdf7c8e7362283b3dbff7a566
Waihon Yew <yewwaihon@gmail.com>
2021-08-18 23:59:17 +0800

    Review model to calcualte stars as percent

--- app/models/review.rb
+++ app/models/review.rb
@@ -7,4 +7,8 @@ class Review < ApplicationRecord
   STARS = [1, 2, 3, 4, 5]
   validates :stars, inclusion: { in: STARS,
     message: "must be between 1 and 5" }
+
+  def stars_as_percent
+    (stars / 5.0) * 100.0
+  end
 end
