458a388ab897750151917914449f78abb4bcdb07
Waihon Yew <yewwaihon@gmail.com>
2021-08-28 11:36:45 +0800

    Update spec to expect saving review from new page

--- spec/features/create_review_spec.rb
+++ spec/features/create_review_spec.rb
@@ -1,12 +1,32 @@
 require 'rails_helper'
 
 describe "Creating a new review" do
-  it "saves the review" do
+  it "saves the review from movie show page" do
     movie = Movie.create(movie_attributes)
 
     visit movie_url(movie)
 
     expect(page).to have_selector("input[type=submit][value='Post Review']")
+
+    fill_in "Name", with: "Roger Ebert"
+    choose("review_stars_3")
+    fill_in "Comment", with: "I laughed, I cried, I spilled my popcorn!"
+
+    click_button "Post Review"
+
+    expect(current_path).to eq(movie_reviews_path(movie))
+
+    expect(page).to have_text("Thanks for your review!")
+  end
+
+  it "saves the review from review new page" do
+    movie = Movie.create(movie_attributes)
+
+    visit movie_reviews_url(movie)
+
+    click_link "Write Review"
+
+    expect(current_path).to eq(new_movie_review_path(movie))
 
     fill_in "Name", with: "Roger Ebert"
     choose("review_stars_3")
