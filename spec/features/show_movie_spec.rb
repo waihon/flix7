ddbfa2cf2585e3c642b8b733c3629dc961fc6ba4
Waihon Yew <yewwaihon@gmail.com>
2021-08-28 22:10:00 +0800

    Update show movie spec to expect display of average stars or 'No reviews'

--- spec/features/show_movie_spec.rb
+++ spec/features/show_movie_spec.rb
@@ -17,5 +17,23 @@ describe "Viewing an individual movie" do
     expect(page).to have_link("Edit")
     expect(page).to have_link("Delete")
     expect(page).to have_selector("input[type=submit][value='Post Review']")
+    expect(page).to have_text("No reviews")
+  end
+
+  it "shows the average number of review stars" do
+    movie = Movie.create(movie_attributes(total_gross: 300_000_000))
+    review1 = movie.reviews.create(review_attributes(stars: 1))
+    review2 = movie.reviews.create(review_attributes(stars: 2))
+    review3 = movie.reviews.create(review_attributes(stars: 3))
+    review4 = movie.reviews.create(review_attributes(stars: 3))
+    review5 = movie.reviews.create(review_attributes(stars: 3))
+    review6 = movie.reviews.create(review_attributes(stars: 4))
+    review7 = movie.reviews.create(review_attributes(stars: 4))
+    review8 = movie.reviews.create(review_attributes(stars: 5))
+
+    visit "http://example.com/movies/#{movie.id}"
+
+    expect(page).to have_link("8 Reviews")
+    expect(page).to have_text("3.1 stars")
   end
 end
