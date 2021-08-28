e9d6f568169253217edb830913a019c2ca4a56c1
Waihon Yew <yewwaihon@gmail.com>
2021-08-29 00:10:55 +0800

    Update show movie spec to expect display of partially-filled stars

--- spec/features/show_movie_spec.rb
+++ spec/features/show_movie_spec.rb
@@ -17,23 +17,24 @@ describe "Viewing an individual movie" do
     expect(page).to have_link("Edit")
     expect(page).to have_link("Delete")
     expect(page).to have_selector("input[type=submit][value='Post Review']")
-    expect(page).to have_text("No reviews")
+    front_stars_style = page.find('div.front-stars')['style']
+    expect(front_stars_style).to eq("width: 0.0%")
   end
 
-  it "shows the average number of review stars" do
+  it "shows the average stars in partially-filled stars" do
     movie = Movie.create(movie_attributes(total_gross: 300_000_000))
-    review1 = movie.reviews.create(review_attributes(stars: 1))
-    review2 = movie.reviews.create(review_attributes(stars: 2))
+    review1 = movie.reviews.create(review_attributes(stars: 5))
+    review2 = movie.reviews.create(review_attributes(stars: 4))
     review3 = movie.reviews.create(review_attributes(stars: 3))
-    review4 = movie.reviews.create(review_attributes(stars: 3))
+    review4 = movie.reviews.create(review_attributes(stars: 4))
     review5 = movie.reviews.create(review_attributes(stars: 3))
-    review6 = movie.reviews.create(review_attributes(stars: 4))
+    review6 = movie.reviews.create(review_attributes(stars: 3))
     review7 = movie.reviews.create(review_attributes(stars: 4))
-    review8 = movie.reviews.create(review_attributes(stars: 5))
 
     visit "http://example.com/movies/#{movie.id}"
 
-    expect(page).to have_link("8 Reviews")
-    expect(page).to have_text("3.1 stars")
+    expect(page).to have_link("7 Reviews")
+    front_stars_style = page.find('div.front-stars')['style']
+    expect(front_stars_style).to eq("width: 74.285714285714286%")
   end
 end
