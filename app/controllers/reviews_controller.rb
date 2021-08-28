1560fbfd628fd8d4f348e4db8b507adbdb682bb3
Waihon Yew <yewwaihon@gmail.com>
2021-08-28 17:39:53 +0800

    Define an action to delete a review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -19,6 +19,12 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy
+    redirect_to movie_reviews_url(@movie), notice: "Review successfully deleted!"
+  end
+
 private
 
   def review_params
