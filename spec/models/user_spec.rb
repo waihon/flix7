c8de10f46c4688035296850516d493918e101165
Waihon Yew <yewwaihon@gmail.com>
2021-09-03 12:53:59 +0800

    Create a model spec for user

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,80 @@
+require 'rails_helper'
+
+describe "A user" do
+  it "requires a name" do
+    user = User.new(name: "")
+
+    user.valid? # populates errors
+
+    expect(user.errors[:name].any?).to eq(true)
+  end
+
+  it "requires an email" do
+    user = User.new(email: "")
+    
+    user.valid?
+
+    expect(user.errors[:email].any?).to eq(true)
+  end
+
+  it "accepts properly formatted email address" do
+    emails = %w[user@example.com first.last@example.com]
+    emails.each do |email|
+      user = User.new(email: email)
+      user.valid?
+      expect(user.errors[:email].any?).to eq(false)
+    end
+  end
+
+  it "rejects improperly formatted email address" do
+    emails = %w[@ user@ @example.com]
+    emails.each do |email|
+      user = User.new(email)
+      user.valid?
+      expect(user.errors[:email].any?).to eq(true) 
+    end
+  end
+
+  it "is valid with example attributes" do
+    user = User.new(user_attributes)
+
+    expect(user.valid?).to eq(true)
+  end
+
+  it "requires a password" do
+    user = User.new(password: "")
+
+    user.valid?
+
+    expect(user.errors[:password].any?).to eq(true)
+  end
+
+  it "requires a password confirmation when a password is present" do
+    user = User.new(password: "secret", password_confirmation: "nomatch")
+
+    user.valid?
+
+    expect(user.errors[:password_confirmation].first).to eq("doesn't match Password")
+  end
+
+  it "requires a password and matching password confirmation when creating" do
+    user = User.create!(user_attributes(password: "secret", password_confirmation: "secret"))
+
+    expect(user.valid?).to eq(true)
+  end
+  
+  it "does not require a password when updating" do
+    user = User.create!(user_attributes)
+
+    user.password = ""
+
+    expect(user.valid?).to eq(true)
+  end
+
+  it "automatically encrypts the password into the password_digest attribute" do
+    user = User.new(password: "secret")
+
+    expect(user.password_digest.present?).to eq(true)
+    
+  end
+end
