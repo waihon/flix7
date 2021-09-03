c7cc16469237fa6d0eb276fa37010876d4fe6dd8
Waihon Yew <yewwaihon@gmail.com>
2021-09-03 16:06:53 +0800

    User model requires a unique, case insensitive email address

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true
-  validates :email, format: { with: /\S+@\S+/ }
+  validates :email, format: { with: /\S+@\S+/ },
+                    uniqueness: { case_sensitive: false }
   has_secure_password
 end
