9fc0c8e589860cc47cf0840c2d27a5d5157dd6bd
Waihon Yew <yewwaihon@gmail.com>
2021-09-03 12:53:05 +0800

    Specify the types of spec to be generated when generating a new scaffold

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,16 @@ module Flix6
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        model_specs:      false,
+        view_specs:       false,
+        controller_specs: false,
+        routing_specs:    false,
+        request_specs:    false,
+        fixtures:         false,
+        helper_specs:     false
+    end
   end
 end
