92c39b5a141c9c4c2539d7d7d24d55dfee09c8e4
Waihon Yew <yewwaihon@gmail.com>
2021-09-04 14:14:02 +0800

    Describe spec for viewing the list of users

---
+++ spec/features/list_users_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe "Viewing the list of users" do
+  it "shows the users" do
+    CREATED_AGO = "created less than a minute ago"
+
+    user1 = User.create!(user_attributes(name: "Larry", email: "larry@example.com"))
+    user2 = User.create!(user_attributes(name: "Moe", email: "moe@example.com"))
+    user3 = User.create!(user_attributes(name: "Curly", email: "curly@example.com"))
+
+    visit users_url
+
+    expect(page).to have_text("3 Users")
+    within find("#user-#{user1.id}") do
+      expect(page).to have_link(user1.name)
+      expect(page).to have_text(CREATED_AGO)
+    end
+    within find("#user-#{user2.id}") do
+      expect(page).to have_link(user2.name)
+      expect(page).to have_text(CREATED_AGO)
+    end
+    within find("#user-#{user3.id}") do
+      expect(page).to have_link(user3.name)
+      expect(page).to have_text(CREATED_AGO)
+    end
+  end
+end
