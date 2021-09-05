2650530a737887e92de7a304e1c1e2d3ffc9c180
Waihon Yew <yewwaihon@gmail.com>
2021-09-05 21:36:39 +0800

    Avoid showing both Email can't be blank and Email is invalid

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   validates :name, presence: true
   validates :email, presence: true
-  validates :email, email: { mode: :strict }
+  validates :email, email: { mode: :strict, allow_blank: true }
   validates :email, uniqueness: { case_sensitive: false }
   has_secure_password
   validates :password, length: { minimum: 10, allow_blank: true }
