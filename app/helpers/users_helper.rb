d566dcf952d44d5102adf273d510c19b85557a9e
Waihon Yew <yewwaihon@gmail.com>
2021-09-05 22:30:34 +0800

    Define a helper to return a user's profile image

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,9 @@ module UsersHelper
   def button_value(user)
     user.new_record? ? "Create Account" : "Update Account"
   end
+
+  def profile_image(user)
+   url = "https://secure.gravatar.com/avatar/#{user.gravatar_id}"
+   image_tag(url, id: "profile-image", alt: user.name)
+  end
 end
