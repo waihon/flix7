21e7fceefe7e03930961be590df7fee88f8f365f
Waihon Yew <yewwaihon@gmail.com>
2021-09-12 15:28:38 +0800

    Permit username for user params

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,6 +43,6 @@ private
 
   def user_params
     params.require(:user).
-      permit(:name, :email, :password, :password_confirmation)
+      permit(:name, :email, :username, :password, :password_confirmation)
   end
 end
