d8799a4078ad0deed60cfa219fb6a829d8e2d721
Waihon Yew <yewwaihon@gmail.com>
2021-09-15 07:48:38 +0800

    Describe users controller spec with context 'when not signed in'

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+describe UsersController do
+  before do
+    @user = User.create!(user_attributes)
+  end
+
+  context "when not signed in" do
+    before do
+      session[:user_id] = nil
+    end
+
+    it "cannot access index" do
+      get :index
+
+      expect(response).to redirect_to(new_session_url)
+    end
+
+    it "cannot access show" do
+      get :show, params: { id: @user }
+
+      expect(response).to redirect_to(new_session_url)
+    end
+
+    it "cannot access edit" do
+      get :edit, params: { id: @user }
+      
+      expect(response).to redirect_to(new_session_url)
+    end
+
+    it "cannot access update" do
+      #patch :update, params: { id: @user }
+      patch :update, params: { id: @user }
+
+      expect(response).to redirect_to(new_session_url)
+    end
+
+    it "cannot access destroy" do
+      delete :destroy, params: { id: @user }
+
+      expect(response).to redirect_to(new_session_url)
+    end
+  end
+end
