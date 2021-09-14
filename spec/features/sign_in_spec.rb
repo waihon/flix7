a612ff70ba135d4d6e3d75e8247b040064fe7725
Waihon Yew <yewwaihon@gmail.com>
2021-09-14 22:55:28 +0800

    Update sign in spec to sign in using username in addition to email

--- spec/features/sign_in_spec.rb
+++ spec/features/sign_in_spec.rb
@@ -22,7 +22,7 @@ describe "Signing in" do
 
     expect(current_path).to eq(signin_path)
 
-    fill_in "Email", with: user.email
+    fill_in "Email or username", with: user.email
     fill_in "Password", with: user.password
 
     click_button "Sign In"
@@ -40,7 +40,43 @@ describe "Signing in" do
 
     expect(current_path).to eq(signin_path)
 
-    fill_in "Email", with: user.email
+    fill_in "Email or username", with: user.email
+    fill_in "Password", with: "no match"
+
+    click_button "Sign In"
+
+    expect(page).to have_text("Invalid email/password combination!")
+    expect(current_path).not_to eq(user_path(user))
+  end
+
+  it "signs in the user if the username/password combination is valid" do
+    user = User.create!(user_attributes)
+
+    visit root_url
+
+    click_link "Sign In"
+
+    expect(current_path).to eq(signin_path)
+
+    fill_in "Email or username", with: user.username
+    fill_in "Password", with: user.password
+
+    click_button "Sign In"
+
+    expect(current_path).to eq(user_path(user))
+    expect(page).to have_text("Welcome back, #{user.name}!")
+  end
+
+  it "does not sign in the user if the username/password combination is invalid" do
+    user = User.create!(user_attributes)
+
+    visit root_url
+
+    click_link "Sign In"
+
+    expect(current_path).to eq(signin_path)
+
+    fill_in "Email or username", with: user.username
     fill_in "Password", with: "no match"
 
     click_button "Sign In"
