64222b204ff2b496c9479ae05ede9a892371b59d
Waihon Yew <yewwaihon@gmail.com>
2021-09-16 23:57:05 +0800

    Update delete movie spec to sign in as an admin

--- spec/features/delete_movie_spec.rb
+++ spec/features/delete_movie_spec.rb
@@ -1,7 +1,13 @@
 require 'rails_helper'
 
 describe "Deleting a movie" do
+  before do
+    @admin = User.create!(user_attributes(admin: true))
+  end
+
   it "destroys the movie and shows the movie listing without the deleted movie" do
+    sign_in(@admin)
+
     movie = Movie.create(movie_attributes)
 
     visit movie_path(movie)
