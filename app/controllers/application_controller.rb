b6f9e8ff044b74d1c898bca84649708153c3b010
Waihon Yew <yewwaihon@gmail.com>
2021-09-17 00:41:14 +0800

    Define supporting methods to authorize admin user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,4 +21,16 @@ private
   end
 
   helper_method :current_user?
+
+  def current_user_admin?
+    current_user && current_user.admin?
+  end
+
+  helper_method :current_user_admin?
+
+  def require_admin
+    unless current_user_admin?
+      redirect_to root_url, alert: "Unauthorized access!"
+    end
+  end
 end
