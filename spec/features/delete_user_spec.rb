ba72a58caf5054197338e3a2253406e5d51cb694
Waihon Yew <yewwaihon@gmail.com>
2021-09-17 15:33:45 +0800

    Update delete user spec to expect only admin user can delete user accounts

--- spec/features/delete_user_spec.rb
+++ spec/features/delete_user_spec.rb
@@ -1,22 +1,64 @@
 require 'rails_helper'
 
 describe "Deleting a user" do
-  it "destroys the user and redirects to the home page" do
-    user = User.create!(user_attributes)
+  before do
+    @user = User.create!(user_attributes(admin: false))
+      sign_in(@user)
+  end
 
-    sign_in(user)
+  context "when not signed in as an admin user" do
+    it "cannot see Delete Account link for own account" do
+      visit user_path(@user)
 
-    visit user_path(user)
+      expect(page).not_to have_link "Delete Account"
+    end
 
-    click_link "Delete Account"
+    it "cannot see Delete Account link for another acount" do
+      another_user = User.create!(user_attributes(name: "Another User",
+        email: "anotheruser@example.com",
+        username: "anotheruser", admin: false))
 
-    expect(current_path).to eq(root_path)
-    expect(page).to have_text("Account successfully deleted!")
+      visit user_path(another_user)
 
-    visit users_path
+      expect(page).not_to have_link "Delete Account"
+    end
+  end
 
-    expect(page).not_to have_text(user.name)
-    expect(page).not_to have_link("Sign Out")
-    expect(page).to have_link("Sign In")
+  context "when signed in as an admin user" do
+    before do
+      @admin = User.create(user_attributes(name: "Administrator",
+        email: "admin@example.com",
+        username: "admin", admin: true))
+      sign_in(@admin)
+    end
+
+    it "destroys another user and redirects to the home page" do
+      visit user_path(@user)
+
+      click_link "Delete Account"
+
+      expect(current_path).to eq(root_path)
+      expect(page).to have_text("Account successfully deleted!")
+
+      visit users_path
+
+      expect(page).not_to have_text(@user.name)
+      expect(page).to have_link("Sign Out")
+      expect(page).not_to have_link("Sign In")
+    end
+
+    it "destroys own account and auto signed out to the home page" do
+      visit user_path(@admin)
+
+      click_link "Delete Account"
+
+      expect(current_path).to eq(root_path)
+      expect(page).to have_text("Account successfully deleted!")
+
+      expect(page).not_to have_text(@admin.name)
+      expect(page).not_to have_link("Sign Out")
+      expect(page).to have_link("Sign In")
+      expect(page).to have_link("Sign Up")
+    end
   end
 end
