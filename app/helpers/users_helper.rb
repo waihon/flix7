6d903b0a0d1a5cb3ef03237863d70123efd15fc1
Waihon Yew <yewwaihon@gmail.com>
2021-09-18 22:22:48 +0800

    Make profile image supports a size

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -3,8 +3,8 @@ module UsersHelper
     user.new_record? ? "Create Account" : "Update Account"
   end
 
-  def profile_image(user)
-   url = "https://secure.gravatar.com/avatar/#{user.gravatar_id}"
+  def profile_image(user, size=80)
+   url = "https://secure.gravatar.com/avatar/#{user.gravatar_id}?s=#{size}"
    image_tag(url, id: "profile-image", alt: user.name)
   end
 end
