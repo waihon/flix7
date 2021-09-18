c8f1007b9c4c059e1207b6da1cf16646ca8eb3bd
Waihon Yew <yewwaihon@gmail.com>
2021-09-18 15:15:53 +0800

    Update users controller spec to expect a more user friendly signin_url

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -13,31 +13,31 @@ describe UsersController do
     it "cannot access index" do
       get :index
 
-      expect(response).to redirect_to(new_session_url)
+      expect(response).to redirect_to(signin_url)
     end
 
     it "cannot access show" do
       get :show, params: { id: @user }
 
-      expect(response).to redirect_to(new_session_url)
+      expect(response).to redirect_to(signin_url)
     end
 
     it "cannot access edit" do
       get :edit, params: { id: @user }
       
-      expect(response).to redirect_to(new_session_url)
+      expect(response).to redirect_to(signin_url)
     end
 
     it "cannot access update" do
       patch :update, params: { id: @user }
 
-      expect(response).to redirect_to(new_session_url)
+      expect(response).to redirect_to(signin_url)
     end
 
     it "cannot access destroy" do
       delete :destroy, params: { id: @user }
 
-      expect(response).to redirect_to(new_session_url)
+      expect(response).to redirect_to(signin_url)
     end
   end
 
