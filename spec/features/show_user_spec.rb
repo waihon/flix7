8c877c15e4f8402fa3cbb7ec5ab85a9876e28a1f
Waihon Yew <yewwaihon@gmail.com>
2021-09-18 21:15:30 +0800

    Refactor show_user_spec to include excample code within a context

--- spec/features/show_user_spec.rb
+++ spec/features/show_user_spec.rb
@@ -1,31 +1,30 @@
 require 'rails_helper'
 
 describe "Viewing a user's profile page" do
-  it "shows the user's details" do
-    user = User.create!(user_attributes)
+  context "when signed in" do
+    before do
+      @user = User.create!(user_attributes)
+      sign_in(@user)
+    end
+    
+    it "shows the user's details" do
+      visit user_url(@user)
 
-    sign_in(user)
+      expect(page).to have_text(@user.name)
+      expect(page).to have_text(@user.email)
+      expect(page).to have_text(@user.created_at.strftime("%B %Y"))
+      expect(page.find("#profile-image")["src"]).to have_content(@user.gravatar_id)
 
-    visit user_url(user)
+      expect(page).to have_link("Edit Account")
+    end
 
-    expect(page).to have_text(user.name)
-    expect(page).to have_text(user.email)
-    expect(page).to have_text(user.created_at.strftime("%B %Y"))
-    expect(page.find("#profile-image")["src"]).to have_content(user.gravatar_id)
+    it "doesn't show Edit Account link if it's not the signed in user" do
+      user2 = User.create!(user_attributes(email: "user2@example.com",
+                                           username: "user2"))
 
-    expect(page).to have_link("Edit Account")
-  end
+      visit user_url(user2)
 
-  it "doesn't show account management links if not the same as signed in user" do
-    user1 = User.create!(user_attributes(email: "user1@example.com",
-                                         username: "user1"))
-    user2 = User.create!(user_attributes(email: "user2@example.com",
-                                         username: "user2"))
-
-    sign_in(user1)
-
-    visit user_url(user2)
-
-    expect(page).not_to have_link("Edit Account")
+      expect(page).not_to have_link("Edit Account")
+    end
   end
 end
