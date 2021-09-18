40c66432e0c61fd121e445622d49178c377dab7a
Waihon Yew <yewwaihon@gmail.com>
2021-09-18 17:04:54 +0800

    Update edit review spec to associate review with user, and fill in Comment instead of Name

--- spec/features/edit_review_spec.rb
+++ spec/features/edit_review_spec.rb
@@ -1,41 +1,46 @@
 require 'rails_helper'
 
 describe "Editing a review" do
+  before do
+    @user = User.create!(user_attributes)
+    sign_in(@user)
+  end
+
   it "updates the review and shows the review's updated details" do
     movie = Movie.create(movie_attributes)
-    review1 = movie.reviews.create(review_attributes(name: "Roger Ebert"))
-    review2 = movie.reviews.create(review_attributes(name: "Gene Siskel"))
-    review3 = movie.reviews.create(review_attributes(name: "Peter Travers"))
+    review1 = movie.reviews.create(review_attributes(comment: "Loved", user: @user))
+    review2 = movie.reviews.create(review_attributes(comment: "Liked", user: @user))
+    review3 = movie.reviews.create(review_attributes(comment: "Boo!", user: @user))
 
     visit movie_reviews_url(movie)
-    expect(page).to have_text(review1.name)
-    expect(page).to have_text(review2.name)
-    expect(page).to have_text(review3.name)
+    expect(page).to have_text(review1.comment)
+    expect(page).to have_text(review2.comment)
+    expect(page).to have_text(review3.comment)
 
     within find("#review-#{review2.id}") do
       click_link "Edit"
     end
 
     expect(current_path).to eq(edit_movie_review_path(movie, review2))
-    expect(find_field('Name').value).to eq(review2.name)
+    expect(find_field('Comment').value).to eq(review2.comment)
 
-    fill_in "Name", with: "Updated Reviewer Name"
+    fill_in "Comment", with: "Updated comment"
     click_button "Update Review"
 
     expect(current_path).to eq(movie_reviews_path(movie))
-    expect(page).to have_text("Updated Reviewer Name")
+    expect(page).to have_text("Updated comment")
     expect(page).to have_text("Review successfully updated!")
   end
 
   it "does not update the movie if it's invalid" do
     movie = Movie.create(movie_attributes)
-    review1 = movie.reviews.create(review_attributes(name: "Roger Ebert"))
-    review2 = movie.reviews.create(review_attributes(name: "Gene Siskel"))
-    review3 = movie.reviews.create(review_attributes(name: "Peter Travers"))
+    review1 = movie.reviews.create(review_attributes(comment: "Loved", user: @user))
+    review2 = movie.reviews.create(review_attributes(comment: "Liked", user: @user))
+    review3 = movie.reviews.create(review_attributes(comment: "Boo!", user: @user))
 
     visit edit_movie_review_url(movie, review3)
 
-    fill_in "Name", with: " "
+    fill_in "Comment", with: " "
 
     click_button "Update Review"
 
