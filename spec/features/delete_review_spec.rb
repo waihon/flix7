38c5d092a4a0c0d25ba27913ecdc00cf9c3148bf
Waihon Yew <yewwaihon@gmail.com>
2021-09-18 16:57:16 +0800

    Update delete review spec to associate review with user, and expect comment instead of name

--- spec/features/delete_review_spec.rb
+++ spec/features/delete_review_spec.rb
@@ -1,25 +1,30 @@
 require 'rails_helper'
 
 describe "Deleting a review" do
+  before do
+    @user = User.create!(user_attributes)
+    sign_in(@user)
+  end
+
   it "destroys the review and shows the review listing without the deleted review" do
     movie = Movie.create(movie_attributes)
-    review1 = movie.reviews.create(review_attributes(name: "Larry"))
-    review2 = movie.reviews.create(review_attributes(name: "Moe"))
-    review3 = movie.reviews.create(review_attributes(name: "Lucy"))
+    review1 = movie.reviews.create(review_attributes(comment: "Loved", user: @user))
+    review2 = movie.reviews.create(review_attributes(comment: "Liked", user: @user))
+    review3 = movie.reviews.create(review_attributes(comment: "Boo!", user: @user))
 
     visit movie_reviews_url(movie)
 
-    expect(page).to have_text("Larry")
-    expect(page).to have_text("Moe")
-    expect(page).to have_text("Lucy")
+    expect(page).to have_text(review1.comment)
+    expect(page).to have_text(review2.comment)
+    expect(page).to have_text(review3.comment)
     within find("#review-#{review2.id}") do
       click_link "Delete"
     end
 
     expect(current_path).to eq(movie_reviews_path(movie))
-    expect(page).to have_text("Larry")
-    expect(page).not_to have_text("Moe")
-    expect(page).to have_text("Lucy")
+    expect(page).to have_text(review1.comment)
+    expect(page).not_to have_text(review2.comment)
+    expect(page).to have_text(review3.comment)
     expect(page).to have_text("Review successfully deleted!")
   end
 end
