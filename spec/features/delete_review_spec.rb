5f93bf6b468f260b03fcfad279e2ed841ff9601c
Waihon Yew <yewwaihon@gmail.com>
2021-09-19 12:47:11 +0800

    Refactor delete review spec with context 'from movie reviews page'

--- spec/features/delete_review_spec.rb
+++ spec/features/delete_review_spec.rb
@@ -4,27 +4,30 @@ describe "Deleting a review" do
   before do
     @user = User.create!(user_attributes)
     sign_in(@user)
+
+    @movie = Movie.create(movie_attributes)
+    @review1 = @movie.reviews.create(review_attributes(comment: "Loved", user: @user))
+    @review2 = @movie.reviews.create(review_attributes(comment: "Liked", user: @user))
+    @review3 = @movie.reviews.create(review_attributes(comment: "Boo!", user: @user))
   end
 
-  it "destroys the review and shows the review listing without the deleted review" do
-    movie = Movie.create(movie_attributes)
-    review1 = movie.reviews.create(review_attributes(comment: "Loved", user: @user))
-    review2 = movie.reviews.create(review_attributes(comment: "Liked", user: @user))
-    review3 = movie.reviews.create(review_attributes(comment: "Boo!", user: @user))
+  context "from movie reviews page" do
+    it "destroys the review and shows the move reviews listing without the deleted review" do
+      visit movie_reviews_url(@movie)
 
-    visit movie_reviews_url(movie)
+      expect(page).to have_text(@review1.comment)
+      expect(page).to have_text(@review2.comment)
+      expect(page).to have_text(@review3.comment)
 
-    expect(page).to have_text(review1.comment)
-    expect(page).to have_text(review2.comment)
-    expect(page).to have_text(review3.comment)
-    within find("#review-#{review2.id}") do
-      click_link "Delete"
+      within find("#review-#{@review2.id}") do
+        click_link "Delete"
+      end
+
+      expect(current_path).to eq(movie_reviews_path(@movie))
+      expect(page).to have_text(@review1.comment)
+      expect(page).not_to have_text(@review2.comment)
+      expect(page).to have_text(@review3.comment)
+      expect(page).to have_text("Review successfully deleted!")
     end
-
-    expect(current_path).to eq(movie_reviews_path(movie))
-    expect(page).to have_text(review1.comment)
-    expect(page).not_to have_text(review2.comment)
-    expect(page).to have_text(review3.comment)
-    expect(page).to have_text("Review successfully deleted!")
   end
 end
