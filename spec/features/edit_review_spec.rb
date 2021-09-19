8d2ec62bffddb412a317a86125d39194343432fb
Waihon Yew <yewwaihon@gmail.com>
2021-09-19 11:27:13 +0800

    Update edit review spec with context 'from user profile page'

--- spec/features/edit_review_spec.rb
+++ spec/features/edit_review_spec.rb
@@ -44,4 +44,28 @@ describe "Editing a review" do
       expect(page).to have_text("error")
     end
   end
+
+  context "from user profile page" do
+    it "updates the review and shows the review's updated details" do
+      visit user_url(@user)
+
+      expect(page).to have_text(@review1.comment)
+      expect(page).to have_text(@review2.comment)
+      expect(page).to have_text(@review3.comment)
+
+      within find("#review-#{@review2.id}") do
+        click_link "Edit"
+      end
+
+      expect(current_path).to eq(edit_movie_review_path(@movie, @review2))
+      expect(find_field('Comment').value).to eq(@review2.comment)
+
+      fill_in "Comment", with: "Updated comment"
+      click_button "Update Review"
+
+      expect(current_path).to eq(user_path(@user))
+      expect(page).to have_text("Updated comment")
+      expect(page).to have_text("Review successfully updated!")
+    end
+  end
 end
