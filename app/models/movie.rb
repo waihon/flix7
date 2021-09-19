60c2d537b66b30064ceee07c723c887823acc758
Waihon Yew <yewwaihon@gmail.com>
2021-09-19 21:39:32 +0800

    A movie has many fans through favorites with a source of user

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,6 +1,7 @@
 class Movie < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :favorites, dependent: :destroy
+  has_many :fans, through: :favorites, source: :user
 
   validates :title, presence: true
   validates :description, presence: true
