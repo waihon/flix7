621ed679412afd5e736567b27dfafc9210809cdc
Waihon Yew <yewwaihon@gmail.com>
2021-09-19 10:07:55 +0800

    Update list reviews spec to expect Edit and Delete links based on sign-in status

--- spec/features/list_reviews_spec.rb
+++ spec/features/list_reviews_spec.rb
@@ -25,4 +25,68 @@ describe "Viewing a list of reviews" do
     expect(page).not_to have_text(review3.comment)
     expect(page).to have_link("Write Review")
   end
+
+  it "shows Edit and Delete links for reviews written by the signed in user" do
+    movie1 = Movie.create(movie_attributes(title: "Iron Man"))
+
+    user2 = User.create!(user_attributes(name: "Second User", username: "second",
+      email: "second@example.com"))
+    user3 = User.create!(user_attributes(name: "Third User", username: "third",
+      email: "third@example.com"))
+
+    review1 = movie1.reviews.create!(review_attributes(comment: "Loved", user: @user))
+    review2 = movie1.reviews.create!(review_attributes(comment: "Liked", user: user2))
+    review3 = movie1.reviews.create!(review_attributes(comment: "Boo!", user: user3))
+
+    expect(movie1.reviews.count).to eq(3)
+
+    visit movie_reviews_url(movie1)
+
+    within find("#review-#{review1.id}") do
+      expect(page).to have_link("Edit")
+      expect(page).to have_link("Delete")
+    end
+    within find("#review-#{review2.id}") do
+      expect(page).not_to have_link("Edit")
+      expect(page).not_to have_link("Delete")
+    end
+    within find("#review-#{review3.id}") do
+      expect(page).not_to have_link("Edit")
+      expect(page).not_to have_link("Delete")
+    end
+
+    sign_in(user2)
+
+    visit movie_reviews_url(movie1)
+
+    within find("#review-#{review1.id}") do
+      expect(page).not_to have_link("Edit")
+      expect(page).not_to have_link("Delete")
+    end
+    within find("#review-#{review2.id}") do
+      expect(page).to have_link("Edit")
+      expect(page).to have_link("Delete")
+    end
+    within find("#review-#{review3.id}") do
+      expect(page).not_to have_link("Edit")
+      expect(page).not_to have_link("Delete")
+    end
+
+    sign_in(user3)
+
+    visit movie_reviews_url(movie1)
+
+    within find("#review-#{review1.id}") do
+      expect(page).not_to have_link("Edit")
+      expect(page).not_to have_link("Delete")
+    end
+    within find("#review-#{review2.id}") do
+      expect(page).not_to have_link("Edit")
+      expect(page).not_to have_link("Delete")
+    end
+    within find("#review-#{review3.id}") do
+      expect(page).to have_link("Edit")
+      expect(page).to have_link("Delete")
+    end
+  end
 end
