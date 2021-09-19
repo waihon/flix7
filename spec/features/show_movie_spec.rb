9b9635e8c5a5e286570d6bc3e631dada8dc327fb
Waihon Yew <yewwaihon@gmail.com>
2021-09-19 22:41:30 +0800

    Refactor show movie spec to move code examples not depending on sign-in status

--- spec/features/show_movie_spec.rb
+++ spec/features/show_movie_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 describe "Viewing an individual movie" do
+  before do
+    @user = User.create(user_attributes(admin: false))
+  end
+
   it "shows the movie's details" do
     movie = Movie.create(movie_attributes(total_gross: 300_000_000))
 
@@ -16,6 +20,23 @@ describe "Viewing an individual movie" do
     expect(page).to have_link("0 Reviews")
     front_stars_style = page.find('div.front-stars')['style']
     expect(front_stars_style).to eq("width: 0.0%")
+  end
+
+  it "shows the average stars in partially-filled stars" do
+    movie = Movie.create(movie_attributes(total_gross: 300_000_000))
+    review1 = movie.reviews.create(review_attributes(stars: 5, user: @user))
+    review2 = movie.reviews.create(review_attributes(stars: 4, user: @user))
+    review3 = movie.reviews.create(review_attributes(stars: 3, user: @user))
+    review4 = movie.reviews.create(review_attributes(stars: 4, user: @user))
+    review5 = movie.reviews.create(review_attributes(stars: 3, user: @user))
+    review6 = movie.reviews.create(review_attributes(stars: 3, user: @user))
+    review7 = movie.reviews.create(review_attributes(stars: 4, user: @user))
+
+    visit movie_url(movie)
+
+    expect(page).to have_link("7 Reviews")
+    front_stars_style = page.find('div.front-stars')['style']
+    expect(front_stars_style).to eq("width: 74.285714285714286%")
   end
 
   context "when not signed in" do
@@ -42,25 +63,7 @@ describe "Viewing an individual movie" do
 
   context "when signed in but not as an admin user" do
     before do
-      @user = User.create(user_attributes(admin: false))
       sign_in(@user)
-    end
-
-    it "shows the average stars in partially-filled stars" do
-      movie = Movie.create(movie_attributes(total_gross: 300_000_000))
-      review1 = movie.reviews.create(review_attributes(stars: 5, user: @user))
-      review2 = movie.reviews.create(review_attributes(stars: 4, user: @user))
-      review3 = movie.reviews.create(review_attributes(stars: 3, user: @user))
-      review4 = movie.reviews.create(review_attributes(stars: 4, user: @user))
-      review5 = movie.reviews.create(review_attributes(stars: 3, user: @user))
-      review6 = movie.reviews.create(review_attributes(stars: 3, user: @user))
-      review7 = movie.reviews.create(review_attributes(stars: 4, user: @user))
-
-      visit "http://example.com/movies/#{movie.id}"
-
-      expect(page).to have_link("7 Reviews")
-      front_stars_style = page.find('div.front-stars')['style']
-      expect(front_stars_style).to eq("width: 74.285714285714286%")
     end
 
     it "doesn't show Edit and Delete links" do
@@ -88,7 +91,8 @@ describe "Viewing an individual movie" do
 
   context "when signed in as an admin user" do
     before do
-      @admin = User.create!(user_attributes(admin: true))
+      @admin = User.create!(user_attributes(name: "Admin", username: "admin",
+        email: "admin@example.com", admin: true))
       sign_in(@admin)
     end
 
