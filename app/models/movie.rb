02337d4c8b90d3dbebdc1932aec2d3540d57d2d9
Waihon Yew <yewwaihon@gmail.com>
2021-09-20 00:42:43 +0800

    A movie has many critics through reviews with user as source

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,6 @@
 class Movie < ApplicationRecord
   has_many :reviews, dependent: :destroy
+  has_many :critics, through: :reviews, source: :user
   has_many :favorites, dependent: :destroy
   has_many :fans, through: :favorites, source: :user
 
