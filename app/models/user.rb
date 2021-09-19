27c89c418988461235b0688676105cd5f1637661
Waihon Yew <yewwaihon@gmail.com>
2021-09-19 20:48:30 +0800

    A user has many dependent favorites

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   has_many :reviews, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   validates :name, presence: true
   validates :email, presence: true
