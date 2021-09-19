765e92523d6659a75a23932be878daf5d631fd04
Waihon Yew <yewwaihon@gmail.com>
2021-09-19 12:07:03 +0800

    Update show user spec to expect number of review stars as percentage

--- spec/features/show_user_spec.rb
+++ spec/features/show_user_spec.rb
@@ -28,31 +28,48 @@ describe "Viewing a user's profile page" do
     end
 
     it "show all the reviews this user has written" do
+      user2 = User.create!(user_attributes(name: "Second User",
+        username: "second", email: "second@example.com"))
+
       movie1 = Movie.create!(movie_attributes(title: "Avengers: Endgame"))
-      review1 = movie1.reviews.create!(review_attributes(comment: "Loved!", user: @user))
+      review1_1 = movie1.reviews.create!(review_attributes(comment: "Loved!", stars: 5, user: @user))
+      review1_2 = movie1.reviews.create!(review_attributes(comment: "Really cool!", stars: 4, user: user2))
+
       movie2 = Movie.create!(movie_attributes(title: "Captain Marvel"))
-      review2 = movie2.reviews.create!(review_attributes(comment: "Liked!", user: @user))
+      review2_1 = movie2.reviews.create!(review_attributes(comment: "Liked!", stars: 3, user: @user))
+      review2_2 = movie2.reviews.create!(review_attributes(comment: "Boo!", stars: 2, user: user2))
+
       movie3 = Movie.create!(movie_attributes(title: "Black Panther"))
-      review3 = movie3.reviews.create!(review_attributes(comment: "Cool!", user: @user))
+      review3_1 = movie3.reviews.create!(review_attributes(comment: "Cool!", stars: 4, user: @user))
+      review3_2 = movie3.reviews.create!(review_attributes(comment: "Liked it!", stars: 3, user: user2))
 
       visit user_url(@user)
 
       expect(page).to have_text("Reviews")
-      expect(page).to have_text("Loved!")
-      expect(page).to have_text("Liked!")
-      expect(page).to have_text("Cool!")
+      expect(page).to have_text(review1_1.comment)
+      expect(page).to have_text(review2_1.comment)
+      expect(page).to have_text(review3_1.comment)
+      expect(page).not_to have_text(review1_2.comment)
+      expect(page).not_to have_text(review2_2.comment)
+      expect(page).not_to have_text(review3_2.comment)
 
-      within find("#review-#{review1.id}") do
+      within find("#review-#{review1_1.id}") do
         expect(page).to have_link("Edit")
         expect(page).to have_link("Delete")
+        front_stars_style = page.find('div.front-stars')['style']
+        expect(front_stars_style).to eq("width: 100.0%")
       end
-      within find("#review-#{review2.id}") do
+      within find("#review-#{review2_1.id}") do
         expect(page).to have_link("Edit")
         expect(page).to have_link("Delete")
+        front_stars_style = page.find('div.front-stars')['style']
+        expect(front_stars_style).to eq("width: 60.0%")
       end
-      within find("#review-#{review3.id}") do
+      within find("#review-#{review3_1.id}") do
         expect(page).to have_link("Edit")
         expect(page).to have_link("Delete")
+        front_stars_style = page.find('div.front-stars')['style']
+        expect(front_stars_style).to eq("width: 80.0%")
       end
     end
 
