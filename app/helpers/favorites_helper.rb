983fef9ba7d02fcc6c58d4799e6cb7d415900479
Waihon Yew <yewwaihon@gmail.com>
2021-09-22 01:09:47 +0800

    Apply style to Fave and Unfave buttons

--- app/helpers/favorites_helper.rb
+++ app/helpers/favorites_helper.rb
@@ -2,9 +2,10 @@ module FavoritesHelper
   def fave_or_unfave_button(movie, favorite)
     if favorite
       button_to "♡ Unfave", movie_favorite_path(movie, favorite),
-        method: :delete
+        method: :delete, class: "button unfave"
     else
-      button_to "♥️ Fave", movie_favorites_path(@movie)
+      button_to "♥️ Fave", movie_favorites_path(@movie),
+        class: "button fave"
     end
   end
 end
