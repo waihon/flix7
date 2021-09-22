34deae20f6daf2d6db89b61c82f07deea0d4c3bf
Waihon Yew <yewwaihon@gmail.com>
2021-09-23 00:04:17 +0800

    Update schema with image file anme in genres by running database migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_22_032834) do
+ActiveRecord::Schema.define(version: 2021_09_22_154230) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2021_09_22_032834) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "image_file_name", default: "placeholder.png"
   end
 
   create_table "movies", force: :cascade do |t|
