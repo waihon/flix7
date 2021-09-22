c7e43d14c42518fcb228459a893dd9c0f0c1376c
Waihon Yew <yewwaihon@gmail.com>
2021-09-22 18:26:06 +0800

    Update edit movie spec to check and uncheck genre checkboxes

--- spec/features/edit_movie_spec.rb
+++ spec/features/edit_movie_spec.rb
@@ -4,10 +4,15 @@ describe "Editing a movie" do
   before do
     @admin = User.create!(user_attributes(admin: true))
     sign_in(@admin)
+
+    @genre1 = Genre.create!(genre_attributes(name: "Action"))
+    @genre2 = Genre.create!(genre_attributes(name: "Comedy"))
+    @genre3 = Genre.create!(genre_attributes(name: "Drama"))
   end
 
   it "updates the movie and shows the movie's updated details" do
     movie = Movie.create(movie_attributes)
+    movie.genres << @genre2
 
     visit movie_url(movie)
     click_link 'Edit'
@@ -16,11 +21,18 @@ describe "Editing a movie" do
     expect(find_field('Title').value).to eq(movie.title)
 
     fill_in "Title", with: "Updated Movie Title"
+    check(@genre1.name)
+    uncheck(@genre2.name)
+    check(@genre3.name)
     click_button "Update Movie"
 
     expect(current_path).to eq(movie_path(movie))
     expect(page).to have_text("Updated Movie Title")
     expect(page).to have_text("Movie successfully updated!")
+
+    expect(page).to have_text(@genre1.name)
+    expect(page).to have_text(@genre3.name)
+    expect(page).not_to have_text(@genre2.name)
   end
 
   it "does not update the movie if it's invalid" do
