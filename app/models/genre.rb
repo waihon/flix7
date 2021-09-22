b653d468be1fe8f5d512ee4271a083c5b75a6445
Waihon Yew <yewwaihon@gmail.com>
2021-09-22 12:35:11 +0800

    A genre has many characterizations and many movies through the join model

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,4 +1,7 @@
 class Genre < ApplicationRecord
+  has_many :characterizations, dependent: :destroy
+  has_many :movies, through: :characterizations
+
   validates :name, presence: true
   validates :name, uniqueness: { case_sensitive: false }
 end
