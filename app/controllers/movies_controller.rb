f5331949ac1e2e2b5467a82ce1e5ac79c4c736e5
Waihon Yew <yewwaihon@gmail.com>
2021-09-22 17:30:21 +0800

    Fetch the movie's associated genres and assign them to an instance variable

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,6 +10,7 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
     @review = @movie.reviews.new
     @fans = @movie.fans
+    @genres = @movie.genres
     if current_user
       @favorite = current_user.favorites.find_by(movie_id: @movie.id)
     end
