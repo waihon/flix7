4ece4127890552d17ce2012b9174374956c8319e
Waihon Yew <yewwaihon@gmail.com>
2021-09-22 11:09:44 +0800

    Describe a genre spec to expect it has a uniqune, case insensitive name

--- spec/models/genre_spec.rb
+++ spec/models/genre_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
-RSpec.describe Genre, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe "A genre" do
+  it "requires a name" do
+    genre = Genre.new(name: "")
+
+    genre.valid?
+
+    expect(genre.errors[:name].any?).to eq(true)
+  end
+
+  it "requires a unique, case insensitive name" do
+    genre1 = Genre.create(name: "Genre 1")
+
+    genre2 = Genre.new(name: genre1.name.upcase)
+    genre2.valid?
+    expect(genre2.errors[:name].first).to eq("has already been taken")
+  end
 end
