5457afc44c15175bf1b0b36a634a843fa69f4814
Waihon Yew <yewwaihon@gmail.com>
2021-09-22 12:34:23 +0800

    A movie has many characterizations and many genres through the join model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,6 +3,8 @@ class Movie < ApplicationRecord
   has_many :critics, through: :reviews, source: :user
   has_many :favorites, dependent: :destroy
   has_many :fans, through: :favorites, source: :user
+  has_many :characterizations, dependent: :destroy
+  has_many :genres, through: :characterizations
 
   validates :title, presence: true
   validates :description, presence: true
