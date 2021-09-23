5e79651559eca5f862678a8b63d9bb9fb1bf62c2
Waihon Yew <yewwaihon@gmail.com>
2021-09-23 23:13:34 +0800

    Define index action that returns genres ordered by name in ascending order

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,4 +1,8 @@
 class GenresController < ApplicationController
+  def index
+    @genres = Genre.order('name ASC')
+  end
+
   def show
     @genre = Genre.find(params[:id])
     @movies = @genre.movies
