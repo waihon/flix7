54b836c23cd0dc3b37f7de948924bc4e724237e6
Waihon Yew <yewwaihon@gmail.com>
2021-09-24 22:28:53 +0800

    All actions except index and show require sign-in and admin authority

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,4 +1,6 @@
 class GenresController < ApplicationController
+  before_action :require_signin, except: [:index, :show]
+  before_action :require_admin, except: [:index, :show]
   def index
     @genres = Genre.order('name ASC')
   end
