889463214d4874414c5c1e75735163f23c3fd052
Waihon Yew <yewwaihon@gmail.com>
2021-09-24 23:05:52 +0800

    Define edit and update actions to update a genre

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -23,6 +23,19 @@ class GenresController < ApplicationController
     end
   end
 
+  def edit
+    @genre = Genre.find(params[:id])
+  end
+
+  def update
+    @genre = Genre.find(params[:id])
+    if @genre.update(genre_params)
+      redirect_to @genre, notice: "Genre successfully updated!"
+    else
+      render :edit
+    end
+  end
+
 private
 
   def genre_params
