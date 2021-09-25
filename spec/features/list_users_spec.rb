3e3d848b4358821b1ac9af23388e87de44fec9f8
Waihon Yew <yewwaihon@gmail.com>
2021-09-25 16:57:52 +0800

    Update list users spec to expect only non-admin users are shown

--- spec/features/list_users_spec.rb
+++ spec/features/list_users_spec.rb
@@ -1,29 +1,42 @@
 require 'rails_helper'
 
 describe "Viewing the list of users" do
-  it "shows the users" do
+  it "shows non-admin users" do
     CREATED_AGO = "created less than a minute ago"
 
-    user1 = User.create!(user_attributes(name: "Larry", email: "larry@example.com", username: "larry"))
-    user2 = User.create!(user_attributes(name: "Moe", email: "moe@example.com", username: "moe"))
-    user3 = User.create!(user_attributes(name: "Curly", email: "curly@example.com", username: "curly"))
+    user1 = User.create!(user_attributes(name: "Larry", email: "larry@example.com",
+      username: "larry", admin: false))
+    user2 = User.create!(user_attributes(name: "Moe", email: "moe@example.com",
+      username: "moe", admin: false))
+    user3 = User.create!(user_attributes(name: "Curly", email: "curly@example.com",
+      username: "curly", admin: false))
+    user4 = User.create!(user_attributes(name: "Admin", email: "admin@example.com",
+      username: "admin", admin: true))
 
     sign_in(user1)
 
     visit users_url
 
     expect(page).to have_text("3 Users")
+
+    expect(page).to have_css("#user-#{user1.id}")
     within find("#user-#{user1.id}") do
       expect(page).to have_link(user1.name)
       expect(page).to have_text(CREATED_AGO)
     end
+
+    expect(page).to have_css("#user-#{user1.id}")
     within find("#user-#{user2.id}") do
       expect(page).to have_link(user2.name)
       expect(page).to have_text(CREATED_AGO)
     end
+
+    expect(page).to have_css("#user-#{user3.id}")
     within find("#user-#{user3.id}") do
       expect(page).to have_link(user3.name)
       expect(page).to have_text(CREATED_AGO)
     end
+
+    expect(page).not_to have_css("#user-#{user4.id}")
   end
 end
