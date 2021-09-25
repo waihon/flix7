c92bf0817bb81d5c1cf4eb740a437456c38faeb6
Waihon Yew <yewwaihon@gmail.com>
2021-09-25 16:40:21 +0800

    Declare not_admins scope to return only those users who are not admins, ordered by their name

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,8 @@ class User < ApplicationRecord
 
   scope :by_name, -> { order(name: :asc)  }
 
+  scope :not_admins, -> { by_name.where(admin: false) }
+
   def gravatar_id
     Digest::MD5::hexdigest(email.downcase)
   end
