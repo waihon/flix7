be3e58ac5a251b93515fc42e9c41f067b3421de4
Waihon Yew <yewwaihon@gmail.com>
2021-09-25 17:56:21 +0800

    Declare a parameterized past_n_days scope to return reviews written in that past n days

--- app/models/review.rb
+++ app/models/review.rb
@@ -8,6 +8,8 @@ class Review < ApplicationRecord
   validates :stars, inclusion: { in: STARS,
     message: "must be between 1 and 5" }
 
+  scope :past_n_days, ->(days=7) { where("created_at >= ?", days.days.ago).order(created_at: :desc) }
+
   def stars_as_percent
     (stars / 5.0) * 100.0
   end
