959f2d403951131dc5f589175d3adbb07ffb293c
Waihon Yew <yewwaihon@gmail.com>
2021-09-25 17:54:51 +0800

    Update review spec to expect result returned by past_n_days scope

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,6 +1,34 @@
 require 'rails_helper'
 
 describe "A review" do
+  context "past_n_days query" do
+    before do
+      movie = Movie.create!(movie_attributes)
+      user = User.create!(user_attributes)
+      @review1 = movie.reviews.create!(review_attributes(created_at: 90.days.ago, user: user))
+      @review2 = movie.reviews.create!(review_attributes(created_at: 60.days.ago, user: user))
+      @review3 = movie.reviews.create!(review_attributes(created_at: 30.days.ago, user: user))
+      @review4 = movie.reviews.create!(review_attributes(created_at: 29.days.ago, user: user))
+      @review5 = movie.reviews.create!(review_attributes(created_at: 7.days.ago, user: user))
+      @review6 = movie.reviews.create!(review_attributes(created_at: 6.days.ago, user: user))
+      @review7 = movie.reviews.create!(review_attributes(created_at: 3.days.ago, user: user))
+      @review8 = movie.reviews.create!(review_attributes(created_at: 1.days.ago, user: user))
+      @review9 = movie.reviews.create!(review_attributes(created_at: 1.hours.ago, user: user))
+    end
+
+    it "returns reviews written in the past specified number of days ordered with the most recent review first" do
+      # The expected result doesn't include @review3 because by the time the query is run, "30 days ago" 
+      # would have become "more than 30 days ago".
+      expect(Review.past_n_days(30)).to eq([@review9, @review8, @review7, @review6, @review5, @review4])
+    end
+
+    it "returns reviews written in the past default of 7 days ordered with the most recent review first" do
+      # The expected result doesn't include @review5 because by the time the query is run, "7 days ago" 
+      # would have become "more than 7 days ago".
+      expect(Review.past_n_days).to eq([@review9, @review8, @review7, @review6])
+    end
+  end
+
   it "belongs to a movie" do
     movie = Movie.create(movie_attributes)
 
