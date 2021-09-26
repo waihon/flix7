7b9ac95b22ea3fc89ca79dc5fc3ffba362bf15eb
Waihon Yew <yewwaihon@gmail.com>
2021-09-26 20:53:01 +0800

    Replaced hardcoded URL with that returned by route helper

--- spec/features/show_movie_spec.rb
+++ spec/features/show_movie_spec.rb
@@ -8,7 +8,7 @@ describe "Viewing an individual movie" do
   it "shows the movie's details" do
     movie = Movie.create(movie_attributes(total_gross: 300_000_000))
 
-    visit "http://example.com/movies/#{movie.id}"
+    visit movie_url(movie)
 
     expect(page).to have_text(movie.title)
     expect(page).to have_text("2008")
