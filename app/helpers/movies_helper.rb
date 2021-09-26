7fb1618e97756d3fa5d9ae39cfef2eeaee4389fa
Waihon Yew <yewwaihon@gmail.com>
2021-09-26 10:04:34 +0800

    Define a view helper to return a link with or without 'active' class baseed on current_page?

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -10,4 +10,12 @@ module MoviesHelper
   def year_of(movie)
     movie.released_on.year
   end
+
+  def nav_link_to(text, url)
+    if current_page?(url)
+      link_to text, url, class: "active"
+    else
+      link_to text, url
+    end
+  end
 end
