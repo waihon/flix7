54e57e65b0d53f743aaf814d6f7ce3c1cb4e1d9e
Waihon Yew <yewwaihon@gmail.com>
2021-09-26 14:13:33 +0800

    Define literal routes for shorter URL without filter such as movies/hits

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "movies#index"
-  get "movies/filter/:filter" => "movies#index", as: "filtered_movies"
+
+  get "movies/hits" => "movies#index", filter: "hits", as: "hits_movies"
+  get "movies/flops" => "movies#index", filter: "flops", as: "flops_movies"
+  get "movies/upcoming" => "movies#index", filter: "upcoming", as: "upcoming_movies"
+  get "movies/recent" => "movies#index", filter: "recent", as: "recent_movies"
+
   resources :movies do
     resources :reviews
     resources :favorites, only: [:create, :destroy]
