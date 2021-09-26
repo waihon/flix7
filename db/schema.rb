a493cb01993332d85037be10ac61cdeb6d75bd46
Waihon Yew <yewwaihon@gmail.com>
2021-09-26 18:19:19 +0800

    Update schema to reflect slug as a new column of movies by running database migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_22_154230) do
+ActiveRecord::Schema.define(version: 2021_09_26_095400) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,6 +51,7 @@ ActiveRecord::Schema.define(version: 2021_09_22_154230) do
     t.string "director"
     t.string "duration"
     t.string "image_file_name", default: "placeholder.png"
+    t.string "slug"
   end
 
   create_table "reviews", force: :cascade do |t|
