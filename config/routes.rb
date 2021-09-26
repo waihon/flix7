0be6c57078dd4cc4a6df3ac90d5356a433efb70b
Waihon Yew <yewwaihon@gmail.com>
2021-09-26 15:16:25 +0800

    Define one route with multiple filters using a constrained :filter parameter

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "movies#index"
 
-  %w(hits flops upcoming recent).each do |filter|
-    get "movies/#{filter}" => redirect("/movies/filter/#{filter}")
-  end
-  get "movies/filter/:filter" => "movies#index", as: "filtered_movies"
+  get "movies/:filter" => "movies#index",
+    constraints: { filter: /hits|flops|upcoming|recent/ },
+    as: :filtered_movies
 
   resources :movies do
     resources :reviews
