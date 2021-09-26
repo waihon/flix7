c12bdc38a8778de503f079e6c79878bf4782064d
Waihon Yew <yewwaihon@gmail.com>
2021-09-26 21:40:30 +0800

    Update schema's version by running database migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_26_095400) do
+ActiveRecord::Schema.define(version: 2021_09_26_133256) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
