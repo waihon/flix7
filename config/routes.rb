45e6c3d8c789522228f10f5140165f4d4c9cbd39
Waihon Yew <yewwaihon@gmail.com>
2021-09-26 14:25:43 +0800

    Dynamically declare a set of routes for shorter URLs without filter

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "movies#index"
 
-  get "movies/hits" => "movies#index", filter: "hits", as: "hits_movies"
-  get "movies/flops" => "movies#index", filter: "flops", as: "flops_movies"
-  get "movies/upcoming" => "movies#index", filter: "upcoming", as: "upcoming_movies"
-  get "movies/recent" => "movies#index", filter: "recent", as: "recent_movies"
+  %w(hits flops upcoming recent).each do |filter|
+    get "movies/#{filter}" => "movies#index", filter: filter, as: "#{filter}_movies"
+  end
 
   resources :movies do
     resources :reviews
