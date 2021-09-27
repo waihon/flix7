c540ef10cbbd8132171a650d71affa23d44dbb70
Waihon Yew <yewwaihon@gmail.com>
2021-09-27 23:13:59 +0800

    Run database migration which also updates schema to reflect removal of image_file_name from movies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_26_140303) do
+ActiveRecord::Schema.define(version: 2021_09_27_144142) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,7 +51,6 @@ ActiveRecord::Schema.define(version: 2021_09_26_140303) do
     t.date "released_on"
     t.string "director"
     t.string "duration"
-    t.string "image_file_name", default: "placeholder.png"
     t.string "slug"
   end
 
