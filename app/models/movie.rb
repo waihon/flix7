21dfff24ee08fa02e761d8701cd8422e9ddf4d22
Waihon Yew <yewwaihon@gmail.com>
2021-09-28 23:50:39 +0800

    Validate that the type of an image file must be a JPEG or PNG

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -69,5 +69,10 @@ private
     unless main_image.blob.byte_size <= 1.megabyte
       errors.add(:main_image, "is too big")
     end
+
+    acceptable_types = ["image/jpeg", "image/png"]
+    unless acceptable_types.include?(main_image.content_type)
+      errors.add(:main_image, "must be a JPEG or PNG")
+    end
   end
 end
