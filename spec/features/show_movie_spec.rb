1e4852d0a3b4b2f630f62e244c2e6e32049f9dee
Waihon Yew <yewwaihon@gmail.com>
2021-09-29 23:54:13 +0800

    Update show movie spec to replace image_file_name in movie_attributes with attaching image files

--- spec/features/show_movie_spec.rb
+++ spec/features/show_movie_spec.rb
@@ -7,6 +7,7 @@ describe "Viewing an individual movie" do
 
   it "shows the movie's details" do
     movie = Movie.create(movie_attributes(total_gross: 300_000_000))
+    movie.main_image.attach(assets_image("ironman.png"))
 
     visit movie_url(movie)
 
