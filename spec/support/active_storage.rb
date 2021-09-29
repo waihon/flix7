de168959f1bd3d491beb200f88a97e151713c172
Waihon Yew <yewwaihon@gmail.com>
2021-09-29 23:26:27 +0800

    Define helper methods for attaching various files

---
+++ spec/support/active_storage.rb
@@ -0,0 +1,24 @@
+def png_file
+  { io: File.open(Rails.root.join('spec', 'files', 'image.png')), filename: 'image.png', content_type: 'image/png' }
+end
+
+def jpg_file
+  { io: File.open(Rails.root.join('spec', 'files', 'image.jpg')), filename: 'image.jpg', content_type: 'image/jpg' }
+end
+
+def gif_file
+  { io: File.open(Rails.root.join('spec', 'files', 'image.gif')), filename: 'image.gif', content_type: 'image/gif' }
+end
+
+def tiff_file
+  { io: File.open(Rails.root.join('spec', 'files', 'image.tiff')), filename: 'image.tiff', content_type: 'image/tiff' }
+end
+
+def big_image_file
+  # https://eoimages.gsfc.nasa.gov/images/imagerecords/74000/74393/world.topo.200407.3x5400x2700.png
+  { io: File.open(Rails.root.join('spec', 'files', 'big_image.png')), filename: 'big_image.png', content_type: 'image/png' }
+end
+
+def pdf_file
+  { io: File.open(Rails.root.join('spec', 'files', 'document.pdf')), filename: 'document.pdf', content_type: 'application/pdf' }
+end
