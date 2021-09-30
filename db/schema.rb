b3efaddbc8344e386ef3c45769d30523c513cdd9
Waihon Yew <yewwaihon@gmail.com>
2021-09-30 23:42:02 +0800

    Update schema to reflect removal of image_file_name from genres by running database migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_27_144844) do
+ActiveRecord::Schema.define(version: 2021_09_30_153727) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -65,7 +65,6 @@ ActiveRecord::Schema.define(version: 2021_09_27_144844) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "image_file_name", default: "placeholder.png"
     t.string "slug"
   end
 
