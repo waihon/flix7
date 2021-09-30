7a878be1e7d7cba47967efd6b49481bfdf6ff0ce
Waihon Yew <yewwaihon@gmail.com>
2021-10-01 00:18:19 +0800

    Promote main_image helper to application level

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def main_image(record)
+    if record.main_image.attached?
+      image_tag record.main_image.variant(resize_to_limit: [150, nil])
+    else
+      image_tag "placeholder.png"
+    end
+  end
 end
--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -18,12 +18,4 @@ module MoviesHelper
       link_to text, url
     end
   end
-
-  def main_image(movie)
-    if movie.main_image.attached?
-      image_tag movie.main_image.variant(resize_to_limit: [150, nil])
-    else
-      image_tag "placeholder.png"
-    end
-  end
 end
