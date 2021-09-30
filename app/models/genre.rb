54ea49eb0c12901a98511cbc62989b56761a2c78
Waihon Yew <yewwaihon@gmail.com>
2021-10-01 00:05:58 +0800

    A genre has one Active Storage attachment named main_image

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -4,6 +4,8 @@ class Genre < ApplicationRecord
   has_many :characterizations, dependent: :destroy
   has_many :movies, through: :characterizations
 
+  has_one_attached :main_image
+
   validates :name, presence: true
   validates :name, uniqueness: { case_sensitive: false }
 
