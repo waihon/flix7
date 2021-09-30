7ec54331ec138fe7b9ef58d448d91190347de370
Waihon Yew <yewwaihon@gmail.com>
2021-10-01 01:06:12 +0800

    Validate image size cannot over 1 MB and of valid content type

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -9,6 +9,8 @@ class Genre < ApplicationRecord
   validates :name, presence: true
   validates :name, uniqueness: { case_sensitive: false }
 
+  validate :acceptable_image
+
   def to_param
     slug
   end
@@ -17,5 +19,18 @@ private
 
   def set_slug
     self.slug = name.parameterize
+  end
+
+  def acceptable_image
+    return unless main_image.attached?
+
+    unless main_image.blob.byte_size <= 1.megabyte
+      errors.add(:main_image, "is too big")
+    end
+
+    acceptable_types = ["image/jpeg", "image/png"]
+    unless acceptable_types.include?(main_image.content_type)
+      errors.add(:main_image, "must be a JPEG or PNG")
+    end
   end
 end
