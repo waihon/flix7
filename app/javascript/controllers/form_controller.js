0168afb7f8fd398a4b2c52d197a5e7d0230ca521
Waihon Yew <yewwaihon@gmail.com>
2022-08-13 20:50:59 +0800

    Define initialize event and submit action of form controller

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -1,7 +1,21 @@
 import { Controller } from "@hotwired/stimulus"
+import debounce from "debounce";
 
 // Connects to data-controller="form"
 export default class extends Controller {
-  connect() {
+  initialize() {
+    // The way the debounce function works is you pass it a function
+    // and it returns a debounced version of that function.
+    // `bind(this)` is a fun little JavaScript thing.
+    // The second argument is how long to wait in milliseconds, and
+    // by default, that's 100 milliseconds.
+    this.submit = debounce(this.submit.bind(this), 300)
+  }
+
+  // The event that triggered a Stimulus action is passed to the
+  // action as the first argument.
+  submit(event) {
+    // Alternative: this.element.requestSubmit();
+    event.target.form.requestSubmit();
   }
 }
