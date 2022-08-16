7b420784d979ccd1c3675ca228a301f17a0897d1
Waihon Yew <yewwaihon@gmail.com>
2022-08-16 08:44:10 +0800

    Add check_parameters as filter is now part of params

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -12,7 +12,12 @@ module MoviesHelper
   end
 
   def nav_link_to(text, url)
-    if current_page?(url)
+    # Adding check_parameters because the filter is now part of the params,
+    # and not part of the path.
+    # Index/Released: http://localhost:3000/movies
+    # Upcoming: http://localhost:3000/movies?filter=upcoming
+    # https://apidock.com/rails/ActionView/Helpers/UrlHelper/current_page%3f
+    if current_page?(url, check_parameters: true)
       link_to text, url, class: "active"
     else
       link_to text, url
