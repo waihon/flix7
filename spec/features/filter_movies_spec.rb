0cdd80cdbbec93babad6cdcfe92360ff00a4db7d
Waihon Yew <yewwaihon@gmail.com>
2022-08-16 08:23:26 +0800

    Expect current path to have a filter params

--- spec/features/filter_movies_spec.rb
+++ spec/features/filter_movies_spec.rb
@@ -8,7 +8,7 @@ describe "Filtering movies" do
 
     click_link "Hits"
 
-    expect(current_path).to eq(filtered_movies_path(:hits))
+    expect(page).to have_current_path(movies_path(filter: :hits))
 
     expect(page).to have_text(movie.title)
   end
@@ -20,7 +20,7 @@ describe "Filtering movies" do
 
     click_link "Flops"
 
-    expect(current_path).to eq(filtered_movies_path(:flops))
+    expect(page).to have_current_path(movies_path(filter: :flops))
 
     expect(page).to have_text(movie.title)
   end
@@ -32,7 +32,7 @@ describe "Filtering movies" do
 
     click_link "Upcoming"
 
-    expect(current_path).to eq(filtered_movies_path(:upcoming))
+    expect(page).to have_current_path(movies_path(filter: :upcoming))
 
     expect(page).to have_text(movie.title)
   end
@@ -44,7 +44,7 @@ describe "Filtering movies" do
 
     click_link "Recent"
 
-    expect(current_path).to eq(filtered_movies_path(:recent))
+    expect(page).to have_current_path(movies_path(filter: :recent))
 
     expect(page).to have_text(movie.title)
   end  
