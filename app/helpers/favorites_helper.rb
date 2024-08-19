1d5d4fb98f51f81b601c857b166d3edf4564aa10
Waihon Yew <yewwaihon@gmail.com>
2024-08-19 20:45:43 +0800

    Display buttons and forms not allowed for non-signed in user with tooltip

--- app/helpers/favorites_helper.rb
+++ app/helpers/favorites_helper.rb
@@ -1,11 +1,17 @@
 module FavoritesHelper
-  def fave_or_unfave_button(movie, favorite)
+  def fave_or_unfave_button(movie, favorite, disabled=false)
+    html_options = { disabled: disabled }
+    if disabled
+      html_options[:data] = { toggle: "tooltip", placement: "botton" }
+      html_options[:title] = "Please sign in first"
+    end
+
     if favorite
-      button_to "♡ Unfave", movie_favorite_path(movie, favorite),
-        method: :delete, class: "button unfave"
+      html_options = html_options.merge({ method: :delete, class: "button unfave" })
+      button_to "♡ Unfave", movie_favorite_path(movie, favorite), html_options
     else
-      button_to "♥️ Fave", movie_favorites_path(@movie),
-        class: "button fave"
+      html_options = html_options.merge({ class: "button fave" })
+      button_to "♥️ Fave", movie_favorites_path(@movie), html_options
     end
   end
 end
